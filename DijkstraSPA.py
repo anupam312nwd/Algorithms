f88b5031db45d50a3e9824998c348d451ed04228
anupam312nwd <anupam312nwd@users.noreply.github.com>
2020-01-03 20:49:56 -0500

    removed graph structure comments considered earlier

--- DijkstraSPA.py
+++ DijkstraSPA.py
@@ -41,13 +41,11 @@ def dijkstra(graph, vertex):
         if X == {vertex}:
             for v in VminusX:
                 heapq.heappush(H, heap[v][0])
-
         # print(H)
         S = heapq.heappop(H)
         v = S[1][0]
         w = S[1][1]
         dist[v] = S[0]
-        # print(v,w)
         # print('v: ', v, 'and w: ', w)
         X = X.union({v})
         VminusX = set(V)-set(X)
@@ -57,52 +55,29 @@ def dijkstra(graph, vertex):
                     T = H[j]
                     H.remove(T)
                     break
+
             if dist[v]+graph[v][u] == min(T[0], dist[v]+graph[v][u]) and T[0] != dist[v]+graph[v][u]:
                 path[u].append(v) # to calculate the path
             heapq.heappush(H, (min(T[0], dist[v]+graph[v][u]), (u, T[1][1])))
             # print(H, v, u)
-        # print(H)
     for v in V:
         path[v].append(v)       # adding own vertex at the end of the path
 
     return dist, path
 
-
 graph = {
     'a': {'b':1, 'c':2, 'd':4},
-    'b': {'e':4},
+    'b': {'d':2, 'e':4},
     'c': {'d':3},
-    'd': {'e':1, 'b':2},
+    'd': {'e':1},
     'e': {}
 }
-
-# graph = {
-#     'a': {'b':1, 'c':2, 'd':4},
-#     'b': {'d':2},
-#     'c': {'d':3},
-#     'd': {},
-# }
-
-# graph = {
-#     'a': [(1,'b'), (2,'c'), (4,'d')],
-#     'b': [(2,'d'), (4,'e')],
-#     'c': [(3,'d')],
-#     'd': [(1,'e')],
-#     'e': []
-# }
 
 dist, path = dijkstra(graph, 'a')
 print(dist, '\n', path)
 
 '''
-Graph structure:
-graph = {
-    'A': [('B',2), ('C',4)],
-    'B': [('C',2)],
-    'C': [],
-}
-
-print(graph)
-print('length of graph[\'A\']:=', len(graph['A']))
-print(graph['A'][0][1])
+Output:
+{'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4} 
+ {'a': ['a', 'a'], 'b': ['a', 'b'], 'c': ['a', 'c'], 'd': ['a', 'b', 'd'], 'e': ['a', 'b', 'd', 'e']}
 '''
